b9d831ff9e3819c7255729a798a667cf9166b968
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2014-02-27 17:52:28 +1000

    Remove old files.

--- index.js
+++
@@ -1,8 +0,0 @@
-var fmn = require('./lib/fmn');
-var report = require('./lib/report');
-
-var resultList = fmn('test/fixtures');
-var reportResult = report(resultList);
-reportResult.messageList.forEach(function(item){
-	console.log(item.message);
-});
