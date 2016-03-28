057da21319f02660bad1a51a056267388f87d8c9
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2016-03-28 12:53:51 +1000

    feat(npm): add support for npm scoped modules

--- tasks/lib/task.js
+++ tasks/lib/task.js
@@ -1,28 +1,10 @@
-var fs = require('fs');
-
 var fmn = require('../../lib/fmn.js');
 var report = require('../../lib/report.js');
-var bowerConfig = require('bower-config');
 
 module.exports = function(options){
-	var dirs = [];
-	if (options.dirs){
-		dirs = dirs.concat(options.dirs)
-	}
-	
-	// Only add bower and npm directories if they exist.
-	var npmDir = 'node_modules';
-	if (options.npm && fs.existsSync(npmDir)){
-		dirs.push(npmDir);
-	}
-	
-	if (options.bower){
-		// Look up the configured bower directory.
-		var bower = bowerConfig.read();
-		if (bower && fs.existsSync(bower.directory)){
-			dirs.push(bower.directory);
-		}
-	}
+	var dirs = options.dirs || [];
+	var fmnDirs = fmn.collectDirs(options)
+	dirs = dirs.concat(fmnDirs);
 	
 	// Scan each of the directories - aggregate the output.
 	var resultList = fmn(dirs);
